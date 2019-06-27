5d9a2b01cb793a12309fffb9af301cb144cbf1eb
wxlzmt <11326806+wxlzmt@users.noreply.github.com>
2019-06-27 10:48:33 +0800

    JFinalConfig的实例对象由spring接管,同时方便切入配置参数.

--- src/main/java/io/github/wxlzmt/sbjd/RunSpringBootJfinalDemoApplication.java
+++ src/main/java/io/github/wxlzmt/sbjd/RunSpringBootJfinalDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
 import com.jfinal.core.JFinalFilter;
 
@@ -15,6 +16,8 @@ import io.github.wxlzmt.sbjd.config.JfinalMainConfig;
 @EnableDiscoveryClient
 @Configuration
 public class RunSpringBootJfinalDemoApplication {
+
+    private static org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(RunSpringBootJfinalDemoApplication.class);
 
     public static void main(String[] args) {
         if (args == null || args.length == 0) {//加这个if是为了方便在集成开发环境下运行,即:默认dev环境
@@ -27,11 +30,12 @@ public class RunSpringBootJfinalDemoApplication {
      * 添加filter
      */
     @Bean
-    public FilterRegistrationBean<JFinalFilter> filterRegistration() {
+    public FilterRegistrationBean<JFinalFilter> filterRegistration(JfinalMainConfig jfinalMainConfig) {
+        LOG.info("注册jfinal filter");
+        Assert.notNull(jfinalMainConfig, "jfinalMainConfig is NULL!");
         FilterRegistrationBean<JFinalFilter> registration = new FilterRegistrationBean<JFinalFilter>();
-        registration.setFilter(new JFinalFilter());
+        registration.setFilter(new JFinalFilter(jfinalMainConfig));
         registration.addUrlPatterns("/*");
-        registration.addInitParameter("configClass", JfinalMainConfig.class.getName());
         registration.setName("jfinal");
         registration.setOrder(1);
         return registration;
--- src/main/java/io/github/wxlzmt/sbjd/config/JfinalMainConfig.java
+++ src/main/java/io/github/wxlzmt/sbjd/config/JfinalMainConfig.java
@@ -1,5 +1,10 @@
 package io.github.wxlzmt.sbjd.config;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -13,20 +18,24 @@ import com.jfinal.template.Engine;
 
 import io.github.wxlzmt.sbjd.controller.IndexController;
 
-public class JfinalMainConfig extends JFinalConfig {
+@Component
+public class JfinalMainConfig extends JFinalConfig implements InitializingBean {
 
-    private boolean isDevMode = true;
+    private static org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(JfinalMainConfig.class);
+
+    @Autowired
+    private Environment environment;
+
+    private Boolean isDev = null;
 
     @Override
     public void configConstant(Constants me) {
-        me.setDevMode(true);
-
+        me.setDevMode(this.isDevMode());
     }
 
     @Override
     public void configRoute(Routes me) {
         me.add("/", IndexController.class);
-
         // 自动绑定
         // 1.如果没用加入注解，必须以Controller结尾,自动截取前半部分为key
         // 2.加入ControllerBind的 获取 key
@@ -42,7 +51,8 @@ public class JfinalMainConfig extends JFinalConfig {
 
     @Override
     public void configPlugin(Plugins me) {
-        if (!isDevMode) {// 这个if是为了展示下述示例代码切保证不运行它也不报错
+        if ("true".equalsIgnoreCase(environment.getProperty("jfinal.database.enable", "false"))) {// 这个if是为了展示下述示例代码切保证不运行它也不报错
+            LOG.info("初始化数据库...");
             // 配置C3p0数据库连接池插件
             C3p0Plugin c3p0Plugin = null;
             //String dbType = "mysql";
@@ -55,7 +65,6 @@ public class JfinalMainConfig extends JFinalConfig {
             // 配置ActiveRecord插件
             ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
             me.add(arp);
-
             //动态扫描Model并绑定
             new AutoBindModels(arp);
         }
@@ -64,15 +73,26 @@ public class JfinalMainConfig extends JFinalConfig {
     @Override
     public void configInterceptor(Interceptors me) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void configHandler(Handlers me) {
-
         //静态资源不经过jfinal
         me.add(new UrlSkipHandler("^/actuator(/.*)?|.*.js|.*.css|.*.json|.*.png|.*.gif|.*.jpg|.*.jpeg|.*.bmp|.*.ico|.*.exe|.*.txt|.*.zip|.*.rar|.*.7z$", false));
+    }
 
+    public boolean isDevMode() {
+        return this.isDev;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        LOG.info("调用" + this.getClass().getName() + ".afterPropertiesSet()");
+        if (isDev == null) {
+            String act = environment.getProperty("spring.profiles.active", "dev");
+            this.isDev = !"prod".equalsIgnoreCase(act);
+            LOG.info("当前是否是devMode====>" + this.isDev);
+        }
     }
 
 }
